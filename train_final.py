90f2832209bf50d7e1a9f4a583d230c0bc250470
Hchib1204 <127296264+Hchib1204@users.noreply.github.com>
2026-03-09 19:32:37 +0530

    Add files via upload

---
+++ train_final.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "112a180b-ccb0-4d1b-9b3d-56a768d0018b",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
